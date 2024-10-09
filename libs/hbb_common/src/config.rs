42cb8f6ce9bf0c6061aaca9f103c36d1954a9a7f
ycphoenix <30286497+ycphoenix@users.noreply.github.com>
2024-10-09 14:28:29 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["home.yc0.win"];
+pub const PUBLIC_RS_PUB_KEY: &str = "+Wz8EUm6jDIgXtinIu9jDOCFzDU710HJCB+NzwkQrJg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
